57e29ca74e912fb9bd34b206b5abc583395dbed8
fadel Diakhate <fadeldiakhate95@gmail.com>
2022-01-28 16:31:21 +0100

    ajout+alerte

--- scripts/formulaire.js
+++ scripts/formulaire.js
@@ -1,4 +1,5 @@
   function soumettre(form) {
+      alert("formulaire envoyé: ")
       console.log(form.firstname.value);
       console.log(form.lastname.value);
       console.log(form.country.value);
